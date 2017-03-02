26e19a25362cc1115b1ba1677a54c1ccd3465338
macabset <maija.absetz@helsinki.fi>
2017-03-02 09:49:56 +0200

    final A
    datafile. how to get data folder?

---
+++ human_data2.R
@@ -0,0 +1,8 @@
+#Setting the needed packages and working directory to my computer:
+
+library(Matrix)
+library(ggplot2)
+library(dplyr)
+
+setwd(C:/Users/Petri/Documents/GitHub/IODS-final/data)
+getwd()
